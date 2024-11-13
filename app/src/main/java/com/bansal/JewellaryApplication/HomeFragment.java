e70d0cd47785b04b0c0076db7bea12489a866f24
Parth200611 <parthdahapute7@gmail.com>
2024-11-13 20:31:43 +0530

    all update done CategorywisesubcategoryActivity added categor in HomeFragment added

--- app/src/main/java/com/bansal/JewellaryApplication/HomeFragment.java
+++ app/src/main/java/com/bansal/JewellaryApplication/HomeFragment.java
@@ -1,7 +1,9 @@
 package com.bansal.JewellaryApplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -19,6 +21,7 @@ import java.util.ArrayList;
 public class HomeFragment extends Fragment {
 
 ImageSlider Imageslider,Imagesliver2;
+CardView cvgold,cvcoinsorbar,cvloosedtone,cvloosediamond,cvplatinum,cvslivery,cvgoldjewellary;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,6 +30,65 @@ ImageSlider Imageslider,Imagesliver2;
         View view= inflater.inflate(R.layout.fragment_home, container, false);
         Imageslider = view.findViewById(R.id.isUserHomeFragmentOfferImageslider);
         Imagesliver2 = view.findViewById(R.id.isUserHomeFragmentOfferImageslider2);
+        cvgold=view.findViewById(R.id.cvUserFragmentGold);
+        cvcoinsorbar=view.findViewById(R.id.cvUserFragmentCoins);
+        cvloosedtone=view.findViewById(R.id.cvUserFragmentloosestone);
+
+        cvloosediamond=view.findViewById(R.id.cvUserFragmentLooseDiamond);
+        cvplatinum=view.findViewById(R.id.cvUserFragmentPlatinum);
+        cvslivery=view.findViewById(R.id.cvUserFragmentsliver);
+
+        cvgoldjewellary =view.findViewById(R.id.cvUserFragmentGoldjewellary);
+
+        cvgold.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(),CategorywisesubcategoryActivity.class);
+                startActivity(i);
+            }
+        });
+        cvcoinsorbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(),CategorywisesubcategoryActivity.class);
+                startActivity(i);
+            }
+        });
+        cvloosedtone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(),CategorywisesubcategoryActivity.class);
+                startActivity(i);
+            }
+        });
+        cvloosediamond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(),CategorywisesubcategoryActivity.class);
+                startActivity(i);
+            }
+        });
+        cvplatinum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(),CategorywisesubcategoryActivity.class);
+                startActivity(i);
+            }
+        });
+        cvslivery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(),CategorywisesubcategoryActivity.class);
+                startActivity(i);
+            }
+        });
+        cvgoldjewellary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(),CategorywisesubcategoryActivity.class);
+                startActivity(i);
+            }
+        });
 
         ArrayList<SlideModel> slideModelsArrayList = new ArrayList<>();
         slideModelsArrayList.add(new SlideModel(R.drawable.off1, ScaleTypes.CENTER_CROP));
